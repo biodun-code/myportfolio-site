6e8494f60df119f65fba55970961117a57a7e284
majorshuga <biodunbiodun0000@gmail.com>
2023-05-26 10:24:51 +0100

    changes

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,39 +1,54 @@
-import React, { useEffect } from 'react'; // eslint-disable-next-line 
-import { SiReact, SiCss3,SiHtml5 } from 'react-icons/si'; // eslint-disable-next-line 
-import {IoLogoJavascript } from 'react-icons/io';
-import { customAos } from '../../utils/customAos';
+import React, { useEffect } from "react"; // eslint-disable-next-line
+import { SiReact, SiCss3, SiHtml5 } from "react-icons/si"; // eslint-disable-next-line
+import { IoLogoJavascript } from "react-icons/io";
+import { customAos } from "../../utils/customAos";
 
-import '../styles/Hero.css';
-import Para from '../common/Para';
+import "../styles/Hero.css";
+import Para from "../common/Para";
 
 const Hero = () => {
-
   useEffect(() => {
     if (window.visualViewport.width > 600) {
-      customAos('pushLeftIcons', '.bLeft', -30)
-      customAos('pushLeftIcons', '.tLeft', -30)
-      customAos('pushRightIcons', '.bRight', -70)
-      customAos('pushRightIcons', '.tRight', -70)
+      customAos("pushLeftIcons", ".bLeft", -30);
+      customAos("pushLeftIcons", ".tLeft", -30);
+      customAos("pushRightIcons", ".bRight", -70);
+      customAos("pushRightIcons", ".tRight", -70);
     }
-  }, [])
+  }, []);
 
-  const heading = <h1 data-aos="fade-down" data-aos-duration="2000">Hello<br /> <span>I'm Toheeb<br />Abdullahi</span></h1>
+  const heading = (
+    <h1 data-aos="fade-down" data-aos-duration="2000">
+      Hello
+      <br />{" "}
+      <span>
+        I'm Toheeb
+        <br />
+        Abdullahi
+      </span>
+    </h1>
+  );
 
   return (
-
-    <div className='Hero'>
+    <div className="Hero">
       <div className="left-col">
         <Para
-          intro={'Introduction'}
+          intro={"Introduction"}
           h1={heading}
-          para={"I am a passionate front-end developer. I have a strong foundation in HTML, CSS, and JavaScript and I am constantly learning and exploring new technologies to enhance my skills. I have experience in developing responsive and user-friendly web interfaces using modern front-end frameworks such as React, Angular, and Vue. I also have experience in working with back-end technologies like Node.js and PHP to develop full-stack web applications.In addition to my technical skills, I am a detail-oriented and creative problem-solver who is committed to delivering high-quality work. I take pride in staying up-to-date with the latest trends and best practices in web development to ensure that my work is always of the highest standard. Feel free to take a look at my portfolio to see some of my latest projects and don't hesitate to get in touch if you think I could be a good fit for your project."}
-          btnClass={'btn'}
-          btn={'Contact Me'} 
-          more={'mailto:biodunbio14@gmail.com'}/>
+          para={
+            "I am a passionate software developer.As a full stack developer, I am proficient in a wide range of technologies, including but not limited to HTML, CSS, JavaScript, React, Node.js, Python, and SQL. My adaptable nature allows me to seamlessly switch between front-end and back-end development, ensuring cohesive and efficient workflows that drive exceptional user experiences.. I have experience in developing responsive and user-friendly web interfaces using modern front-end frameworks such as React, Angular, and Vue. I also have experience in working with back-end technologies like Node.js and PHP to develop full-stack web applications.In addition to my technical skills, I am a detail-oriented and creative problem-solver who is committed to delivering high-quality work. I take pride in staying up-to-date with the latest trends and best practices in web development to ensure that my work is always of the highest standard. Feel free to take a look at my portfolio to see some of my latest projects and don't hesitate to get in touch if you think I could be a good fit for your project."
+          }
+          btnClass={"btn"}
+          btn={"Contact Me"}
+          more={"mailto:biodunbio14@gmail.com"}
+        />
       </div>
       <div className="right-col">
         {/* <div className="ring"></div> */}
-        <div className="person" data-aos="fade-up" data-aos-duration="1800"></div>
+        <div
+          className="person"
+          data-aos="fade-up"
+          data-aos-duration="1800"
+        ></div>
         {/* <div className="icon bLeft" 
           data-aos="zoom-in" data-aos-duration="2100"
         ><SiReact /></div>
@@ -48,8 +63,7 @@ const Hero = () => {
         ><SiCss3 /></div> */}
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -18,7 +18,7 @@ const Skills = () => {
         <Para
           intro={'My Skills'}
           title={'Why Hire Me For Your Next Project?'}
-          para={"Lots of motivation, Consistency, Positive energy and responsibility. Able to efficiently self-manage during independent projects, as well as collaborate,  Experience in building basic infra and CI/CD processes from scratch, Building Full stack web applications with branching strategies ."}
+          para={"With a comprehensive skill set in both front-end and back-end development, I offer a holistic approach to projects. I am adaptable, continuously learning, and committed to staying at the forefront of technology. Collaboration and communication are at the core of my work, ensuring your vision is translated into a successful reality. With a focus on user experience and a passion for delivering high-quality solutions, hiring me means gaining a dedicated partner who will go above and beyond to create exceptional digital experiences. Let's collaborate on your next project and achieve outstanding results together."}
           btn={'Download CV'}
           more={cv} />
 
